a18f2c0195f97b8adf3f37cff6949e706dd91368
Vitaliy7171 <vitaliynekrasov1@gmail.com>
2022-10-04 16:53:07 +0300

    Add markup, styles and swiper for popular section. Add styles for mobile, tablet and desktop version

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// HERO SWIPER //
+
 var swiper = new Swiper('.mySwiper', {
   pagination: {
     el: '.swiper-pagination',
@@ -12,3 +14,97 @@ var swiper = new Swiper('.mySwiper', {
     disableOnInteraction: false,
   },
 });
+
+// POPULAR SWIPER FOR MOBILE //
+
+if (window.innerWidth < 767 && window.innerWidth > 375) {
+  var swiper = new Swiper('.mySwiper-mob', {
+    slidesPerView: 2,
+    spaceBetween: 15,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+}
+
+let widthMatchMobile = window.matchMedia('(min-width: 375px) and (max-width: 767px)');
+widthMatchMobile.addEventListener('change', onWidthChangeMobile);
+function onWidthChangeMobile(evt) {
+  if (evt.matches) {
+    var swiper = new Swiper('.mySwiper-mob', {
+      slidesPerView: 2,
+      spaceBetween: 15,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+}
+
+// POPULAR SWIPER FOR TABLET //
+
+if (window.innerWidth < 1439 && window.innerWidth > 768) {
+  var swiper = new Swiper('.mySwiper-mob', {
+    slidesPerView: 3,
+    spaceBetween: 15,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+}
+
+let widthMatchTablet = window.matchMedia('(min-width: 768px) and (max-width: 1439px)');
+widthMatchTablet.addEventListener('change', onWidthChangeTablet);
+function onWidthChangeTablet(evt) {
+  if (evt.matches) {
+    var swiper = new Swiper('.mySwiper-mob', {
+      slidesPerView: 3,
+      spaceBetween: 15,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+}
+
+// POPULAR SWIPER FOR DESKTOP //
+
+if (window.innerWidth > 1439) {
+  var swiper = new Swiper('.mySwiper-mob', {
+    slidesPerView: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+}
+
+let widthMatchDesktop = window.matchMedia('(min-width: 1440px)');
+widthMatchDesktop.addEventListener('change', onWidthChangeDesktop);
+function onWidthChangeDesktop(evt) {
+  if (evt.matches) {
+    var swiper = new Swiper('.mySwiper-mob', {
+      slidesPerView: 4,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+}
