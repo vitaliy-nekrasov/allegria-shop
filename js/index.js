8f026ec0883192fb0796cbb5b33f6edf165df588
Vitaliy7171 <vitaliynekrasov1@gmail.com>
2022-10-05 14:37:12 +0300

    Update popular section swiper js code

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// HERO SWIPER //
+// HERO SECTION SWIPER //
 
 var swiper = new Swiper('.mySwiper', {
   pagination: {
@@ -15,96 +15,63 @@ var swiper = new Swiper('.mySwiper', {
   },
 });
 
-// POPULAR SWIPER FOR MOBILE //
+// OPTIONS FOR POPULAR SECTION SWIPER //
+
+let options = {
+  slidesPerView: null,
+  spaceBetween: 15,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+};
+
+// POPULAR SECTION SWIPER FOR MOBILE //
 
 if (window.innerWidth < 767 && window.innerWidth > 375) {
-  var swiper = new Swiper('.mySwiper-mob', {
-    slidesPerView: 2,
-    spaceBetween: 15,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  options.slidesPerView = 2;
+  var swiper = new Swiper('.mySwiper-mob', options);
 }
 
 let widthMatchMobile = window.matchMedia('(min-width: 375px) and (max-width: 767px)');
 widthMatchMobile.addEventListener('change', onWidthChangeMobile);
 function onWidthChangeMobile(evt) {
   if (evt.matches) {
-    var swiper = new Swiper('.mySwiper-mob', {
-      slidesPerView: 2,
-      spaceBetween: 15,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+    options.slidesPerView = 2;
+    var swiper = new Swiper('.mySwiper-mob', options);
   }
 }
 
-// POPULAR SWIPER FOR TABLET //
+// POPULAR SECTION SWIPER FOR TABLET //
 
 if (window.innerWidth < 1439 && window.innerWidth > 768) {
-  var swiper = new Swiper('.mySwiper-mob', {
-    slidesPerView: 3,
-    spaceBetween: 15,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  options.slidesPerView = 3;
+  var swiper = new Swiper('.mySwiper-mob', options);
 }
 
 let widthMatchTablet = window.matchMedia('(min-width: 768px) and (max-width: 1439px)');
 widthMatchTablet.addEventListener('change', onWidthChangeTablet);
 function onWidthChangeTablet(evt) {
   if (evt.matches) {
-    var swiper = new Swiper('.mySwiper-mob', {
-      slidesPerView: 3,
-      spaceBetween: 15,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+    options.slidesPerView = 3;
+    var swiper = new Swiper('.mySwiper-mob', options);
   }
 }
 
-// POPULAR SWIPER FOR DESKTOP //
+// POPULAR SECTION SWIPER FOR DESKTOP //
 
 if (window.innerWidth > 1439) {
-  var swiper = new Swiper('.mySwiper-mob', {
-    slidesPerView: 4,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  options.slidesPerView = 4;
+  var swiper = new Swiper('.mySwiper-mob', options);
 }
 
 let widthMatchDesktop = window.matchMedia('(min-width: 1440px)');
 widthMatchDesktop.addEventListener('change', onWidthChangeDesktop);
 function onWidthChangeDesktop(evt) {
   if (evt.matches) {
-    var swiper = new Swiper('.mySwiper-mob', {
-      slidesPerView: 4,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+    options.slidesPerView = 4;
+    var swiper = new Swiper('.mySwiper-mob', options);
   }
 }
